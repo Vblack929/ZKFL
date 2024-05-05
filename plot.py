f73f236ad38deb131a4e93f4b8231f51a3992afe
vblack <t8fu@ucsd.edu>
2024-05-05 20:19:22 +0100

    mac

--- plot.py
+++ plot.py
@@ -2,37 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data for plotting
-ours = np.loadtxt("log/fl_200.txt")
-fl_noise_1 = np.loadtxt("log/noise05.txt")
-fl_noise_2 = np.loadtxt("log/noise1.txt")
-fl_noise_3 = np.loadtxt("log/noise_15.txt")
-# cl = np.loadtxt("cl_200.txt")
+# ours = np.loadtxt("log/fl_200.txt")
+# fl_noise_1 = np.loadtxt("log/noise05.txt")
+# fl_noise_2 = np.loadtxt("log/noise1.txt")
+# fl_noise_3 = np.loadtxt("log/noise_15.txt")
+# # cl = np.loadtxt("cl_200.txt")
+# # plot these accuracy in the same plot
+# fig, ax = plt.subplots()
+# ax.plot(ours, label="Ours")
+# ax.plot(fl_noise_1, label="noise level = 0.5")
+# ax.plot(fl_noise_2, label="noise level = 1")
+# ax.plot(fl_noise_3, label="noise level = 1.5")
+# # ax.plot(cl, label="CL")
+# ax.set(xlabel='Round', ylabel='Accuracy',
+#        )
+# plt.title("CIFAR10")
+# # ax.grid(True)
+# plt.legend()
+# plt.show()
+# fig.savefig("noise.png")
+
+byzantine = np.loadtxt("log/fl_200.txt")
+no_byzantine = np.loadtxt('log/vanilla_acc_200.txt')
 # plot these accuracy in the same plot
 fig, ax = plt.subplots()
-ax.plot(ours, label="Ours")
-ax.plot(fl_noise_1, label="noise level = 0.5")
-ax.plot(fl_noise_2, label="noise level = 1")
-ax.plot(fl_noise_3, label="noise level = 1.5")
-# ax.plot(cl, label="CL")
+ax.plot(no_byzantine, label="No Byzantine")
+ax.plot(byzantine, label="Byzantine")
 ax.set(xlabel='Round', ylabel='Accuracy',
        )
 # ax.grid(True)
 plt.legend()
-plt.show()
-fig.savefig("noise.png")
-
-# byzantine = np.loadtxt("log/fl_200.txt")
-# no_byzantine = np.loadtxt('log/vanilla_acc_200.txt')
-# # plot these accuracy in the same plot
-# fig, ax = plt.subplots()
-# ax.plot(no_byzantine, label="No Byzantine")
-# ax.plot(byzantine, label="Byzantine")
-# ax.set(xlabel='Round', ylabel='Accuracy',
-#        )
-# # ax.grid(True)
-# plt.legend()
-# # plt.show()
-# fig.savefig("byzantine3.png")
+plt.title("CIFAR10")
+# plt.show()
+fig.savefig("byzantine3.png")
 
 
 # save the plot
